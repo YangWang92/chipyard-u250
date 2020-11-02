c34c1270d86f0b3de406fa8c94b7e03c23f9b37a
David Metz <david.metz14@googlemail.com>
2020-11-02 18:03:20 +0100

    update to latest boom master/chipyard dev

--- generators/chipyard/src/main/scala/config/BoomConfigs.scala
+++ generators/chipyard/src/main/scala/config/BoomConfigs.scala
@@ -13,105 +13,109 @@ class SmallBoomConfig extends Config(
 class Nothing extends Config((site, here, up) => {
   case None => None
 })
-
-class SmallInoBoomConfig extends Config(
-  new chipyard.iobinders.WithUARTAdapter ++                      // display UART with a SimUARTAdapter
-  new chipyard.iobinders.WithTieOffInterrupts ++                 // tie off top-level interrupts
-  new chipyard.iobinders.WithBlackBoxSimMem ++                   // drive the master AXI4 memory with a SimAXIMem
-  new chipyard.iobinders.WithTiedOffDebug ++                     // tie off debug (since we are using SimSerial for testing)
-  new chipyard.iobinders.WithSimSerial ++                        // drive TSI with SimSerial for testing
-  new testchipip.WithTSI ++                                      // use testchipip serial offchip link
-  new chipyard.config.WithBootROM ++                             // use default bootrom
-  new chipyard.config.WithUART ++                                // add a UART
-  new chipyard.config.WithL2TLBs(1024) ++                        // use L2 TLBs
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++           // no top-level MMIO master port (overrides default set in rocketchip)
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++          // no top-level MMIO slave port (overrides default set in rocketchip)
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++       // use Sifive L2 cache
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++ // no external interrupts
-  new boom.common.WithSmallInoBooms ++                              // small boom config
-  new boom.common.WithNBoomCores(1) ++                           // single-core boom
-  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++  // hierarchical buses including mbus+l2
-  new freechips.rocketchip.system.BaseConfig)                    // "base" rocketchip system
+//
+//class SmallInoBoomConfig extends Config(
+//  new chipyard.iobinders.WithUARTAdapter ++                      // display UART with a SimUARTAdapter
+//  new chipyard.iobinders.WithTieOffInterrupts ++                 // tie off top-level interrupts
+//  new chipyard.iobinders.WithBlackBoxSimMem ++                   // drive the master AXI4 memory with a SimAXIMem
+//  new chipyard.iobinders.WithTiedOffDebug ++                     // tie off debug (since we are using SimSerial for testing)
+//  new chipyard.iobinders.WithSimSerial ++                        // drive TSI with SimSerial for testing
+//  new testchipip.WithTSI ++                                      // use testchipip serial offchip link
+//  new chipyard.config.WithBootROM ++                             // use default bootrom
+//  new chipyard.config.WithUART ++                                // add a UART
+//  new chipyard.config.WithL2TLBs(1024) ++                        // use L2 TLBs
+//  new freechips.rocketchip.subsystem.WithNoMMIOPort ++           // no top-level MMIO master port (overrides default set in rocketchip)
+//  new freechips.rocketchip.subsystem.WithNoSlavePort ++          // no top-level MMIO slave port (overrides default set in rocketchip)
+//  new freechips.rocketchip.subsystem.WithInclusiveCache ++       // use Sifive L2 cache
+//  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++ // no external interrupts
+//  new boom.common.WithSmallInoBooms ++                              // small boom config
+//  new boom.common.WithNBoomCores(1) ++                           // single-core boom
+//  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++  // hierarchical buses including mbus+l2
+//  new freechips.rocketchip.system.BaseConfig)                    // "base" rocketchip system
 
 class MediumBoomConfig extends Config(
   new boom.common.WithNMediumBooms(1) ++                         // medium boom config
   new chipyard.config.AbstractConfig)
 
-class MediumInoBoomConfig extends Config(
-  new chipyard.iobinders.WithUARTAdapter ++
-  new chipyard.iobinders.WithTieOffInterrupts ++
-  new chipyard.iobinders.WithBlackBoxSimMem ++
-  new chipyard.iobinders.WithTiedOffDebug ++
-  new chipyard.iobinders.WithSimSerial ++
-  new testchipip.WithTSI ++
-  new chipyard.config.WithBootROM ++
-  new chipyard.config.WithUART ++
-  new chipyard.config.WithL2TLBs(1024) ++
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
-  new boom.common.WithMediumInoBooms ++                              // medium boom config
-  new boom.common.WithNBoomCores(1) ++
-  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
-  new freechips.rocketchip.system.BaseConfig)
-
-class MediumSliceBoomConfig extends Config(
-  new chipyard.iobinders.WithUARTAdapter ++
-  new chipyard.iobinders.WithTieOffInterrupts ++
-  new chipyard.iobinders.WithBlackBoxSimMem ++
-  new chipyard.iobinders.WithTiedOffDebug ++
-  new chipyard.iobinders.WithSimSerial ++
-  new testchipip.WithTSI ++
-  new chipyard.config.WithBootROM ++
-  new chipyard.config.WithUART ++
-  new chipyard.config.WithL2TLBs(1024) ++
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
-  new boom.common.WithMediumSliceBooms ++                              // medium boom config
-  new boom.common.WithNBoomCores(1) ++
-  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
-  new freechips.rocketchip.system.BaseConfig)
-
-class MediumCasBoomConfig extends Config(
-  new chipyard.iobinders.WithUARTAdapter ++
-  new chipyard.iobinders.WithTieOffInterrupts ++
-  new chipyard.iobinders.WithBlackBoxSimMem ++
-  new chipyard.iobinders.WithTiedOffDebug ++
-  new chipyard.iobinders.WithSimSerial ++
-  new testchipip.WithTSI ++
-  new chipyard.config.WithBootROM ++
-  new chipyard.config.WithUART ++
-  new chipyard.config.WithL2TLBs(1024) ++
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
-  new boom.common.WithMediumCasBooms ++                              // medium boom config
-  new boom.common.WithNBoomCores(1) ++
-  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
-  new freechips.rocketchip.system.BaseConfig)
-
-class MediumDnbBoomConfig extends Config(
-  new chipyard.iobinders.WithUARTAdapter ++
-  new chipyard.iobinders.WithTieOffInterrupts ++
-  new chipyard.iobinders.WithBlackBoxSimMem ++
-  new chipyard.iobinders.WithTiedOffDebug ++
-  new chipyard.iobinders.WithSimSerial ++
-  new testchipip.WithTSI ++
-  new chipyard.config.WithBootROM ++
-  new chipyard.config.WithUART ++
-  new chipyard.config.WithL2TLBs(1024) ++
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
-  new boom.common.WithMediumDnbBooms ++                              // medium boom config
-  new boom.common.WithNBoomCores(1) ++
-  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
-  new freechips.rocketchip.system.BaseConfig)
+class MediumInoQueueBoomConfig extends Config(
+  new boom.common.WithNMediumInoQueueBooms(1) ++                         // medium boom config
+  new chipyard.config.AbstractConfig)
+//
+//class MediumInoBoomConfig extends Config(
+//  new chipyard.iobinders.WithUARTAdapter ++
+//  new chipyard.iobinders.WithTieOffInterrupts ++
+//  new chipyard.iobinders.WithBlackBoxSimMem ++
+//  new chipyard.iobinders.WithTiedOffDebug ++
+//  new chipyard.iobinders.WithSimSerial ++
+//  new testchipip.WithTSI ++
+//  new chipyard.config.WithBootROM ++
+//  new chipyard.config.WithUART ++
+//  new chipyard.config.WithL2TLBs(1024) ++
+//  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
+//  new freechips.rocketchip.subsystem.WithNoSlavePort ++
+//  new freechips.rocketchip.subsystem.WithInclusiveCache ++
+//  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
+//  new boom.common.WithMediumInoBooms ++                              // medium boom config
+//  new boom.common.WithNBoomCores(1) ++
+//  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
+//  new freechips.rocketchip.system.BaseConfig)
+//
+//class MediumSliceBoomConfig extends Config(
+//  new chipyard.iobinders.WithUARTAdapter ++
+//  new chipyard.iobinders.WithTieOffInterrupts ++
+//  new chipyard.iobinders.WithBlackBoxSimMem ++
+//  new chipyard.iobinders.WithTiedOffDebug ++
+//  new chipyard.iobinders.WithSimSerial ++
+//  new testchipip.WithTSI ++
+//  new chipyard.config.WithBootROM ++
+//  new chipyard.config.WithUART ++
+//  new chipyard.config.WithL2TLBs(1024) ++
+//  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
+//  new freechips.rocketchip.subsystem.WithNoSlavePort ++
+//  new freechips.rocketchip.subsystem.WithInclusiveCache ++
+//  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
+//  new boom.common.WithMediumSliceBooms ++                              // medium boom config
+//  new boom.common.WithNBoomCores(1) ++
+//  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
+//  new freechips.rocketchip.system.BaseConfig)
+//
+//class MediumCasBoomConfig extends Config(
+//  new chipyard.iobinders.WithUARTAdapter ++
+//  new chipyard.iobinders.WithTieOffInterrupts ++
+//  new chipyard.iobinders.WithBlackBoxSimMem ++
+//  new chipyard.iobinders.WithTiedOffDebug ++
+//  new chipyard.iobinders.WithSimSerial ++
+//  new testchipip.WithTSI ++
+//  new chipyard.config.WithBootROM ++
+//  new chipyard.config.WithUART ++
+//  new chipyard.config.WithL2TLBs(1024) ++
+//  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
+//  new freechips.rocketchip.subsystem.WithNoSlavePort ++
+//  new freechips.rocketchip.subsystem.WithInclusiveCache ++
+//  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
+//  new boom.common.WithMediumCasBooms ++                              // medium boom config
+//  new boom.common.WithNBoomCores(1) ++
+//  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
+//  new freechips.rocketchip.system.BaseConfig)
+//
+//class MediumDnbBoomConfig extends Config(
+//  new chipyard.iobinders.WithUARTAdapter ++
+//  new chipyard.iobinders.WithTieOffInterrupts ++
+//  new chipyard.iobinders.WithBlackBoxSimMem ++
+//  new chipyard.iobinders.WithTiedOffDebug ++
+//  new chipyard.iobinders.WithSimSerial ++
+//  new testchipip.WithTSI ++
+//  new chipyard.config.WithBootROM ++
+//  new chipyard.config.WithUART ++
+//  new chipyard.config.WithL2TLBs(1024) ++
+//  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
+//  new freechips.rocketchip.subsystem.WithNoSlavePort ++
+//  new freechips.rocketchip.subsystem.WithInclusiveCache ++
+//  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
+//  new boom.common.WithMediumDnbBooms ++                              // medium boom config
+//  new boom.common.WithNBoomCores(1) ++
+//  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
+//  new freechips.rocketchip.system.BaseConfig)
 
 class LargeBoomConfig extends Config(
   new boom.common.WithNLargeBooms(1) ++                          // large boom config
