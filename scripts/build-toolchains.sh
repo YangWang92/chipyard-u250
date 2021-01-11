5ec2ba37a0483270d361b1df2cff44601ab34bb4
David Metz <david.metz14@googlemail.com>
2021-01-11 10:12:25 +0100

    don't build qemu - fix

--- scripts/build-toolchains.sh
+++ scripts/build-toolchains.sh
@@ -109,7 +109,7 @@ else
         *) false ;;
         esac; ) || die 'obsolete make version; need GNU make 4.x or later'
 
-    # module_prepare riscv-gnu-toolchain qemu
+    module_prepare riscv-gnu-toolchain
     module_build riscv-gnu-toolchain --prefix="${RISCV}" --with-cmodel=medany
     echo '==>  Building GNU/Linux toolchain'
     module_make riscv-gnu-toolchain linux
